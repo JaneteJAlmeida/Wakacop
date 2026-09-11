4072c9ca0d4d3ea4e30b7e5b5d1d7d7a736eea98
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-09-11 20:38:06 -0300

    implements flow repository pauta

---
+++ src/main/java/academy/wakanda/wakacop/pauta/infra/PautaInfraRepository.java
@@ -0,0 +1,23 @@
+package academy.wakanda.wakacop.pauta.infra;
+
+import academy.wakanda.wakacop.pauta.application.service.PautaRepository;
+import academy.wakanda.wakacop.pauta.domain.Pauta;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class PautaInfraRepository implements PautaRepository {
+    private final PautaSpringDataJPARespository pautaSpringDataJPARespository;
+
+    @Override
+    public Pauta salva(Pauta pauta) {
+        log.info("[start] PautaInfraRepository - salva");
+        pautaSpringDataJPARespository.save(pauta);
+        log.info("[finish] PautaInfraRepository - salva");
+        return pauta;
+    }
+}
