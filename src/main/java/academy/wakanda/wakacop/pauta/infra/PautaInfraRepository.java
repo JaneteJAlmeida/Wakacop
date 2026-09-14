215a4f7a32174db856076fdcba88949fb327ce64
JaneteJAlmeida <janebeatriz26@yahoo.com.br>
2026-09-14 19:23:30 -0300

    feat:Valida abertura sessao votacao

--- src/main/java/academy/wakanda/wakacop/pauta/infra/PautaInfraRepository.java
+++ src/main/java/academy/wakanda/wakacop/pauta/infra/PautaInfraRepository.java
@@ -4,7 +4,11 @@ import academy.wakanda.wakacop.pauta.application.service.PautaRepository;
 import academy.wakanda.wakacop.pauta.domain.Pauta;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.boot.models.annotations.internal.NotFoundAnnotation;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Repository
@@ -19,5 +23,14 @@ public class PautaInfraRepository implements PautaRepository {
         pautaSpringDataJPARespository.save(pauta);
         log.info("[finish] PautaInfraRepository - salva");
         return pauta;
+    }
+
+    @Override
+    public Pauta buscaPautaPorId(UUID idPauta) {
+        log.info("[start] PautaInfraRepository = buscaPautaPorId");
+        Pauta pautaPorId = pautaSpringDataJPARespository.findById(idPauta)
+                        .orElseThrow(() -> new RuntimeException("Pauta não encontrada"));
+        log.info("[finish] PautaInfraRepository = buscaPautaPorId");
+        return null;
     }
 }
--- src/main/java/academy/wakanda/wakacop/sessaovotacao/application/service/SessaoVotacaoapplicationService.java
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/application/service/SessaoVotacaoapplicationService.java
@@ -1,5 +1,7 @@
 package academy.wakanda.wakacop.sessaovotacao.application.service;
 
+import academy.wakanda.wakacop.pauta.application.service.PautaService;
+import academy.wakanda.wakacop.pauta.domain.Pauta;
 import academy.wakanda.wakacop.sessaovotacao.application.api.SessaoAberturaRequest;
 import academy.wakanda.wakacop.sessaovotacao.application.api.SessaoAberturaResponse;
 import academy.wakanda.wakacop.sessaovotacao.domain.SessaoVotacao;
@@ -12,11 +14,13 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class SessaoVotacaoapplicationService implements SessaoVotacaoService {
     private final SessaoVotacaoRespository sessaoVotacaoRespository;
+    private final PautaService pautaServie;
 
     @Override
     public SessaoAberturaResponse abreSessao(SessaoAberturaRequest sessaoAberturaRequest) {
         log.info("[start] SessaoVotacaoapplicationService - abreSessao");
-        SessaoVotacao sessaoVotacao = sessaoVotacaoRespository.salva(new SessaoVotacao(sessaoAberturaRequest));
+        Pauta pauta = pautaServie.getPautaPorId(sessaoAberturaRequest.getIdPauta());
+        SessaoVotacao sessaoVotacao = sessaoVotacaoRespository.salva(new SessaoVotacao(sessaoAberturaRequest, pauta));
         log.info("[finish] SessaoVotacaoapplicationService - abreSessao");
         return new SessaoAberturaResponse(sessaoVotacao);
 
